bb40e1ec2de3207bfb7d590859822109075a7ae4
Gabriel <gabriel.gomes@kestraa.com>
2020-05-25 15:36:52 -0300

    This commit make changes on Conta class and creation of getters and setters methods

---
+++ src/Conta.java
@@ -0,0 +1,58 @@
+public class Conta {
+
+    private double saldo;
+    private int agencia;
+    private int numero;
+    private Cliente titular;
+
+    public void depositar(double valor) {
+        this.saldo += valor;
+    }
+
+    public boolean sacar(double valor) {
+        if (this.saldo >= valor) {
+            this.saldo -= valor;
+            return true;
+        }
+        System.out.println("Saldo insuficiente!");
+        return false;
+    }
+
+    public boolean transferir(double valor, Conta destino) {
+        if (this.saldo >= valor) {
+            this.saldo -= valor;
+            destino.depositar(valor);
+            return true;
+        }
+        System.out.println("Saldo insuficiente!");
+        return false;
+    }
+
+    public double getSaldo() {
+        return this.saldo;
+    }
+
+    public int getNumero() {
+        return this.numero;
+    }
+
+    public void setNumero(int novoNumero) {
+        this.numero = novoNumero;
+    }
+
+    public int getAgencia() {
+        return this.agencia;
+    }
+
+    public void setAgencia(int agencia) {
+        this.agencia = agencia;
+    }
+
+    public Cliente getTitular() {
+        return titular;
+    }
+
+    public void setTitular(Cliente titular) {
+        this.titular = titular;
+    }
+}
---
+++ src/TestaGetSet.java
@@ -0,0 +1,15 @@
+public class TestaGetSet {
+
+    public static void main(String[] args) {
+        Conta conta = new Conta();
+        conta.setNumero(1500);
+        System.out.println(conta.getNumero());
+
+        Cliente gabriel = new Cliente();
+        gabriel.setNome("Gabriel Gomes");
+        gabriel.setCpf("999.999.999-99");
+        gabriel.setProfissao("Desenvolvedor");
+        conta.setTitular(gabriel);
+    }
+
+}
